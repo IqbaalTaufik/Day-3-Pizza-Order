a54e920d7eff2f4b0e032023e3406122b251f293
IqbaalTaufik <58700500+IqbaalTaufik@users.noreply.github.com>
2022-11-30 17:19:55 +0000

    a1

--- main.py
+++ main.py
@@ -12,19 +12,17 @@ bill = 0
 if size == "S":
   bill += 15
 elif size == "M":
-    bill += 20
-else:
+  bill += 20
+elif size == "L":
   bill += 25
 
-
-if add_pepperoni =="Y":
+if add_pepperoni == "Y":
   if size == "S":
     bill += 2
-else:
-  bill += 3
+  else:
+    bill += 3
 
-
-if extra_cheese =="Y":
+if extra_cheese == "Y":
   bill += 1
 
 print(f"your bill is : ${bill}")
